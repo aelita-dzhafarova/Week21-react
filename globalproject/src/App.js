d3a109b5f8a767acde2c2bf1a2aae28769034663
aelita-dzhafarova <aelita.dzhafarova@gmail.com>
2023-07-09 23:34:02 +0200

    add chart

---
+++ globalproject/src/App.js
@@ -0,0 +1,31 @@
+import { Chart } from "react-google-charts";
+import './App.css';
+
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Work", 11],
+  ["Eat", 2],
+  ["Commute", 2],
+  ["Watch TV", 2],
+  ["Sleep", 7],
+];
+
+export const options = {
+  title: "My Daily Activities",
+};
+
+function App() {
+  return (
+    <div>
+      <Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width={"100%"}
+        height={"400px"}
+      />
+  </div>
+  );
+}
+
+export default App;
